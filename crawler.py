e97de3baa8bb412d53260b8c9fc099ff5eb85f02
Hao-Kailong <827170110@qq.com>
2022-02-11 17:57:42 +0800

    使用selenium动态爬取

---
+++ crawler.py
@@ -0,0 +1,50 @@
+from selenium.webdriver.common.keys import Keys
+import time
+import json
+
+
+class TirelessCrawler(object):
+    def __init__(self, driver):
+        # selenium驱动器
+        self.driver = driver
+        # 保留所有微博文本
+        self.array = set()
+
+    def crawl_current_content(self):
+        number = 1
+        while True:
+            # 当前微博
+            try:
+                cur = self.driver.find_element_by_xpath("""//*[@id="scroller"]/div[1]/div[{}]""".format(number))
+            except:
+                break
+            # 对应文字区域
+            try:
+                content = cur.find_element_by_class_name("""detail_wbtext_4CRf9""")
+            except Exception as e:
+                print("[WARNING] 找不到微博内容！")
+                raise ValueError
+            try:
+                span = content.find_element_by_class_name("""expand""")
+                span.click()
+                time.sleep(0.1)
+            except:
+                pass
+            finally:
+                print(content.text[:10] + " ...... " + content.text[-10:])
+                self.array.add(content.text)
+            number += 1
+
+    def crawl(self):
+        """展开微博并保存，滑动页面，直到底端"""
+        for i in range(120):
+            self.crawl_current_content()
+            self.driver.find_element_by_tag_name('body').send_keys(Keys.END)
+            time.sleep(1)
+
+    def save_to(self, file):
+        """以JSON格式写入文件"""
+        with open(file, "w", encoding="utf-8") as f:
+            text = json.dumps(list(self.array), ensure_ascii=False)
+            f.write(text)
+
---
+++ spider_dynamic.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+import time
+from crawler import TirelessCrawler
+
+
+'''打开网址，预登陆'''
+driver = webdriver.Chrome()
+print('准备登陆weibo.com')
+# 发送请求
+wait = WebDriverWait(driver, 10)
+driver.get("https://weibo.com/login.php")
+# 重要：暂停1分钟进行预登陆，此处填写账号密码及验证
+time.sleep(60)
+
+
+'''输入关键词到搜索框，完成搜索'''
+print('开始搜索')
+# 使用selector去定位关键词搜索框
+s_input = driver.find_element_by_xpath("""//*[@id="app"]/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div[2]/div/span/form/div/input""")
+# 向搜索框中传入字段
+s_input.send_keys("南海战略态势感知")
+s_input.submit()
+s_input.send_keys(Keys.ENTER)
+# 稍等3秒
+time.sleep(3)
+# 切换窗口
+driver.switch_to.window(driver.window_handles[1])
+
+
+'''点击进入对应用户主页'''
+print("进入用户主页")
+icon = driver.find_element_by_xpath("""//*[@id="pl_feedlist_index"]/div[2]/div[1]/div/div[1]/a""")
+icon.click()
+# 稍等3秒
+time.sleep(3)
+# 切换窗口
+driver.switch_to.window(driver.window_handles[2])
+
+
+'''爬取全部微博'''
+print("开始向下滑动页面，展开微博...")
+tireless = TirelessCrawler(driver)
+tireless.crawl()
+
+
+'''写入文件'''
+print("将所有微博写入文件")
+tireless.save_to("weibo.txt")
+
